1e314aecf7983ff2327dfee915ed2d472d9ad2e8
Georg Duemmler <gd@bergwerk.ag>
2016-05-11 12:09:41 +0200

    cookie js

--- Resources/Private/src/js/app.js
+++ Resources/Private/src/js/app.js
@@ -1,4 +1,19 @@
 $(document).ready(function()
 {
-    alert('test');
+    $('.tx-bwrkcookie form').submit(function()
+    {
+        var cookieValidityDays = $(this).find('input[name="bwrk_cookie_validity_days"]').val();
+        createCookie('bwrk_cookie','accepted', cookieValidityDays);
+        $(this).parents('.tx-bwrkcookie').hide();
+        return false;
+    });
 });
+function createCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+}
